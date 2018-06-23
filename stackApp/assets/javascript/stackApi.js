664192aee6943e0503af9005b8514d99b9eda98d
manukam <maduranga.manuka3@gmail.com>
2018-06-23 23:01:15 +0530

    Auto-generated commit

---
+++ stackApp/assets/javascript/stackApi.js
@@ -0,0 +1,28 @@
+{/* <script type='text/javascript' src='https://api.stackexchange.com/js/2.0/all.js'></script> */ }
+export function apiInit() {
+    SE.init({
+        clientId: 12679,
+        key: 'llMpok01uLwieV8w1UxwCQ((',
+        channelUrl: 'http://localhost:4200/login',
+        complete: function (data) {
+            console.log(data);
+        }
+    });
+}
+
+export function apiAuthenticate() {
+    SE.authenticate({
+        success: function (data) {
+            console.log(data);
+            alert(
+                'User Authorized with account id = ' +
+                data.networkUsers[0].account_id + ', got access token = ' +
+                data.accessToken
+            );
+        },
+        error: function (data) {
+            alert('An error occurred:\n' + data.errorName + '\n' + data.errorMessage);
+        },
+        networkUsers: true
+    });
+}
